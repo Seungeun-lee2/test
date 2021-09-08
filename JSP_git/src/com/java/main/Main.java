dea457eb74bd82e0a4e40cf3f7262c802885dcbf
Seungeun-lee2 <seungeun1010@gmail.com>
2021-09-08 16:34:53 +0900

    add main.java sys.out("spring")

--- JSP_git/src/com/java/main/Main.java
+++ JSP_git/src/com/java/main/Main.java
@@ -3,10 +3,12 @@ package com.java.main;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		System.out.println("main");
 		System.out.println("hello");
 		System.out.println("java");
 		System.out.println("hotfix2 : JSP");
+		System.out.println("Spring");
 		
 	}
 
